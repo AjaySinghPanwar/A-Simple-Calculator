fd87b3bcde40a032b63fc4cf7ad582cb6b374ec7
Ajay Singh Panwar <64411148+AjaySinghPanwar@users.noreply.github.com>
2021-03-13 23:49:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if(this.id=="CE"){
+			var output=reverseNumberFormat(buttonText).toString();
+			if(output){//if output has a value
+				output= output.substr(0,output.length-1);
+			}
+		}
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+
